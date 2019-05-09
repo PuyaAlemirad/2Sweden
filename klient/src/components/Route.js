6908714a46c3dd7b117d48552bf09016bdd0a083
Joakim Andersson <Joakim.Ander@Hotmail.com>
2019-05-09 13:44:49 +0200

    New Client folder

---
+++ klient/src/components/Route.js
@@ -0,0 +1,32 @@
+import React from "react"
+import Segment from "./Segment"
+import {minutesToHM} from "../tools/StringFormating"
+
+export default function Route(props) {
+    return (
+        <div>
+            <h5>{`${minutesToHM(props.duration)} ${props.price}${props.currency}`}</h5>
+
+            <div className='table'>
+                <div className="segment title">
+                    <h5>{"From-To"}</h5>
+                    <h5>{"Transport"}</h5>
+                    <h5>{"Duration"}</h5>
+                    <h5>{`Price(${props.currency})`}</h5>
+                </div>
+
+
+                {props.segments.map(s => <Segment
+                    key={s.index}
+                    transport={s.transport}
+                    from={s.from}
+                    to={s.to}
+                    price={s.price}
+                    duration={s.duration}
+                    currency={s.currency}
+                    blue={s.blueBack}
+                    />)}
+            </div>
+        </div>
+    )
+}
---
+++ klient/src/components/Segment.js
@@ -0,0 +1,14 @@
+import React from "react"
+import {minutesToHM} from "../tools/StringFormating"
+
+export default function Segment(props) {
+    return (
+        <div className={"segment "+ (props.blue? "yellow" : "blue")}>
+
+            <p className="col">{`${props.from}-${props.to}`}</p>
+            <p className="col yellow">{props.transport}</p>
+            <p className="col">{minutesToHM(props.duration)}</p>
+            <p>{`${props.price}`}</p>
+        </div>
+    )
+}
