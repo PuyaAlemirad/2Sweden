8e71d848f6d0a84b2da8147999b04fb3e70b71db
ghuli <ghulammurtaza123@gmail.com>
2019-05-29 10:56:23 +0200

    OS table layout added

--- client/src/components/Main/index.js
+++ client/src/components/Main/index.js
@@ -3,7 +3,9 @@ import Route from "../Route"
 import { get_r2r, get_CC } from "../../tools/DAO"
 
 
+
 export default class Main extends React.Component {
+   
     constructor() {
         super()
         this.state = {
@@ -14,9 +16,10 @@ export default class Main extends React.Component {
             localCurrency:null
         }
 
-        this.search = this.search.bind(this)
-        this.sortRoutes = this.sortRoutes.bind(this)
+      //  this.search = this.search.bind(this)
+     //   this.sortRoutes = this.sortRoutes.bind(this)
     }
+   
     componentDidMount(){
       
     get_CC(curr => {
@@ -29,15 +32,26 @@ export default class Main extends React.Component {
 
     render() {
         let i = 0; //Each Route needs a unique key.
-   
+        const logo = require('./stockholm.jpg');
+        const åre = require('./åre.jpg');
+        const falun = require('./falun.jpg');
+        function content(event){
+             event.target.style.backgroundColor="grey"
+             //const p= document.getElementById("para")
+
+           
+             
+        }
         return (
 
             <main className="main-div">
               
-                <div className="container search">
-                 <form onSubmit={this.search}>
-                    <div className="row sm-flex">
-                        <div className="col-lg-6 col-md-6">
+                <div className="container" >
+                
+                 <form onSubmit={this.search} className="form">
+                     <div className="searchBar">
+                   
+                        <div className="col-lg-12 col-md-12">
                              
                                 <input
                                      required
@@ -46,12 +60,12 @@ export default class Main extends React.Component {
                                      className="search form-control"
                                 />
                         </div>
-                        <div className="col-lg-6 col-md-6 form-group ">                      
-                                 <select name="destination" className="custom-select">
+                        <div className="col-lg-12 col-md-12 form-group ">                      
+                                 {/* <select name="destination" className="custom-select">
                                      <option defaultValue="stockholm">Stockholm</option>
                                     <option value="are">Åre</option>
                                     <option value="falun">Falun</option>
-                                </select>
+                                </select> */}
                                 
 
                                     <select name="currencyCode" className="custom-select">
@@ -83,29 +97,105 @@ export default class Main extends React.Component {
                         </div> 
                         </div> */}
                         
-                         <div className="col-lg-6 col-md-6">
-                             <a href="index.html" className="schedule">See the Olympic Schedule</a>
-                        </div>
+                         
                         <div className="col-lg-6 col-md-6">
                         <button type="submit" className="btn btn-primary btn btn-main ">Search</button>
                         
                          </div>
-                         <div className="col-lg col-md-6 sortBy">
+                         
+                         {/* <div className="col-lg col-md-6 sortBy">
                     <select id="sortBy" onChange={this.sortRoutes}>
                         <option value="sort">Sort By</option>
                         <option value="duration">Duration</option>
                         <option value="price">Price</option>
                         <option value="connections">Connections</option>
                     </select>
-                    </div>
+                    </div> */}
+                    
                         
                     </div>
                    
+                    <table className="olympicTable" border="1px" cellSpacing="50spx" >
+                      <thead>
+                         < tr>
+                         <th></th>
+                         <th>Day 1</th>
+                         <th>Day 2</th>
+                         <th>Day 3</th>
+                         <th>Day 4</th>
+                         <th>Day 5</th>
+                         <th>Day 6</th>
+                         <th>Day 7</th>
+                         <th>Day 8</th>
+                         <th>Day 9</th>
+                         <th>Day 10</th>
+                         <th>Day 11</th>
+                         <th>Day 12</th>
+                         <th>Day 13</th>
+                         <th>Day 14</th>                       
+                          </tr>
+                     </thead>
+                     <tbody>
+                       <tr>
+                           <th><img src={logo} height="100px" width="100px"/></th>
+                           <td onClick={content}></td>
+                           <td onClick={content}>Curling</td>
+                           <td onClick={content}><ul><li>Biathion</li><li> Cross Country Skiing</li><li> Nordic Jumping</li><li> Ski Jumping</li></ul></td>
+                           <td onClick={content}><ul><li>Biathion</li><li> Cross Country Skiing</li><li> Nordic Jumping</li><li> Ski Jumping</li></ul></td>
+                           <td onClick={content}><ul><li>Biathion</li><li> Cross Country Skiing</li><li> Nordic Jumping</li><li> Ski Jumping</li></ul></td>
+                           <td onClick={content}><ul><li>Biathion</li><li> Cross Country Skiing</li><li> Nordic Jumping</li><li> Ski Jumping</li></ul></td>
+                           <td onClick={content}><ul><li>Biathion</li><li> Cross Country Skiing</li><li> Nordic Jumping</li><li> Ski Jumping</li></ul></td>
+                           <td onClick={content}><ul><li>Biathion</li><li> Cross Country Skiing</li><li> Nordic Jumping</li><li> Ski Jumping</li></ul></td>
+                           <td onClick={content}><ul><li>Biathion</li><li> Cross Country Skiing</li><li> Nordic Jumping</li><li> Ski Jumping</li></ul></td>
+                           <td onClick={content}><ul><li>Biathion</li><li> Cross Country Skiing</li><li> Nordic Jumping</li><li> Ski Jumping</li></ul></td>
+                           <td onClick={content}><ul><li>Biathion</li><li> Cross Country Skiing</li><li> Nordic Jumping</li><li> Ski Jumping</li></ul></td>
+                           <td onClick={content}><ul><li>Biathion</li><li> Cross Country Skiing</li><li> Nordic Jumping</li><li> Ski Jumping</li></ul></td>
+                           <td onClick={content}><ul><li>Biathion</li><li> Cross Country Skiing</li><li> Nordic Jumping</li><li> Ski Jumping</li></ul></td>
+                           <td onClick={content}><ul><li>Biathion</li><li> Cross Country Skiing</li><li> Nordic Jumping</li><li> Ski Jumping</li></ul></td>
+                           
+                       </tr>
+                       <tr>
+                       <th><img src={åre} height="100px" width="100px"/></th>
+                           <td onClick={content}><ul><li>Biathion</li><li> Luge</li></ul></td>
+                           <td onClick={content}>Alpline Skiing Luge</td>
+                           <td onClick={content}><ul><li>Biathion</li><li> Cross Country Skiing</li><li> Nordic Jumping</li><li> Ski Jumping</li></ul></td>
+                           <td onClick={content}><ul><li>Biathion</li><li> Cross Country Skiing</li><li> Nordic Jumping</li><li> Ski Jumping</li></ul></td>
+                           <td onClick={content}><ul><li>Biathion</li><li> Cross Country Skiing</li><li> Nordic Jumping</li><li> Ski Jumping</li></ul></td>
+                           <td onClick={content}><ul><li>Biathion</li><li> Cross Country Skiing</li><li> Nordic Jumping</li><li> Ski Jumping</li></ul></td>
+                           <td onClick={content}><ul><li>Biathion</li><li> Cross Country Skiing</li><li> Nordic Jumping</li><li> Ski Jumping</li></ul></td>
+                           <td onClick={content}><ul><li>Biathion</li><li> Cross Country Skiing</li><li> Nordic Jumping</li><li> Ski Jumping</li></ul></td>
+                           <td onClick={content}><ul><li>Biathion</li><li> Cross Country Skiing</li><li> Nordic Jumping</li><li> Ski Jumping</li></ul></td>
+                           <td onClick={content}><ul><li>Biathion</li><li> Cross Country Skiing</li><li> Nordic Jumping</li><li> Ski Jumping</li></ul></td>
+                           <td onClick={content}><ul><li>Biathion</li><li> Cross Country Skiing</li><li> Nordic Jumping</li><li> Ski Jumping</li></ul></td>
+                           <td onClick={content}><ul><li>Biathion</li><li> Cross Country Skiing</li><li> Nordic Jumping</li><li> Ski Jumping</li></ul></td>
+                           <td onClick={content}><ul><li>Biathion</li><li> Cross Country Skiing</li><li> Nordic Jumping</li><li> Ski Jumping</li></ul></td>
+                           <td onClick={content}><ul><li>Biathion</li><li> Cross Country Skiing</li><li> Nordic Jumping</li><li> Ski Jumping</li></ul></td>
+                       </tr>
+                       <tr>
+                       <th><img src={falun} height="130px" width="100px"/></th>
+                           <td onClick={content}><ul><li>Ski Jumping</li></ul></td>
+                           <td onClick={content}><ul><li>Biathion</li><li> Ski Jumping</li></ul></td>
+                           <td onClick={content}><ul><li></li><li> </li></ul></td>
+                           <td onClick={content}><ul><li>Biathion</li><li> Cross Country Skiing</li><li> Ski Jumping</li></ul></td>
+                           <td onClick={content}><ul><li>Biathion</li><li> Cross Country Skiing</li><li> Nordic Combined</li><li> Ski Jumping</li></ul></td>
+                           <td onClick={content}><ul><li>Biathion</li><li> Luge</li></ul></td>
+                           <td onClick={content}><ul><li>Biathion</li><li> Luge</li></ul></td>
+                           <td onClick={content}><ul><li>Biathion</li><li> Luge</li></ul></td>
+                           <td onClick={content}><ul><li>Biathion</li><li> Luge</li></ul></td>
+                           <td onClick={content}><ul><li>Biathion</li><li> Luge</li></ul></td>
+                           <td onClick={content}><ul><li>Biathion</li><li> Luge</li></ul></td>
+                           <td onClick={content}><ul><li>Biathion</li><li> Luge</li></ul></td>
+                           <td onClick={content}><ul><li>Biathion</li><li> Luge</li></ul></td>
+                           <td onClick={content}><ul><li>Biathion</li><li> Luge</li></ul></td>
+                       </tr>
+                       </tbody>
+                    </table>
+                   
                     </form>
-                  
+                    
                 </div>
 
-                <div className="routes">                
+               {/*  <div className="routes">                
 
                     <table className="table">
                         <thead >
@@ -134,12 +224,12 @@ export default class Main extends React.Component {
                       
                     </table>
                 
-                </div>
+                </div> */}
         </main>
         )
     }
 
-    sortRoutes() {
+ /*    sortRoutes() {
         // Sorts state.routes depending on value of element with id: "sortBy"
         let sortAlg
         switch (document.getElementById("sortBy").value) {
@@ -161,9 +251,9 @@ export default class Main extends React.Component {
         this.setState({
             routes: sortedList
         })
-    }
+    } */
 
-    search(form) {
+ /*    search(form) {
         form.preventDefault()
 
         let destination = form.target.elements["destination"].value
@@ -189,5 +279,5 @@ export default class Main extends React.Component {
             this.sortRoutes()
         })
     
-}
+} */
 }
