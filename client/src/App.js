4e1e5cf075decc12db0988a785e95cee09f10ba8
Joakim Andersson <Joakim.Ander@Hotmail.com>
2019-05-30 22:31:27 +0200

    Styled event-picker, It looks alright...

--- client/src/App.js
+++ client/src/App.js
@@ -3,30 +3,24 @@ import './App.css';
 import Header from "./components/Header";
 import Main from "./components/Main";
 import Footer from "./components/Footer";
-const events = require("./tools/Events.json");
 
 
 function App() {
-  // console.log(Object.entries(events).flatMap(e => e[1].events));
   return (
     <div className="container-fluid">
 
       <div className="row">
-          <div className="col-lg col-md col-sm"><Header/></div>
-          </div>
+        <div className="col-lg col-md col-sm"><Header /></div>
+      </div>
 
-      {/* <div className="row">
-      <div className="col-lg col-md col-sm"><Navbar/></div>
-      </div> */}
-
-    <div className="row">
-      <div className="col-lg col-md col-sm"><Main/></div>
-    </div>
+      <div className="row">
+        <div className="col-lg col-md col-sm"><Main /></div>
+      </div>
 
 
-    <div className="row">
-    <div className="col-lg col-md col-sm"><Footer/></div>
-    </div>
+      <div className="row">
+        <div className="col-lg col-md col-sm"><Footer /></div>
+      </div>
     </div>
   );
 }
--- client/src/components/DayOfEvents/index.js
+++ client/src/components/DayOfEvents/index.js
@@ -1,39 +1,51 @@
 import React from "react"
 
+
+function CityButton(props) {
+    let i = 1
+    return (
+        <button type="button" data-toggle="list" className={"list-group-item list-group-item-action"} id={`${props.day}${props.city[0]}`}>
+                {props.events.map(e => 
+                    <div className="d-flex justify-content-between" key={i++}>
+                        <p className="m-0">{e.name}</p>
+                        {(e.isMedal) ? <i className="fas fa-award align-self-center"></i> : ""}
+                    </div>
+                )}
+        </button>
+    )
+} 
+
 export default function DayOfEvents(props) {
 
     return (
-        <div>
-            <h3>{props.date}</h3>
-            <div className="btn-group-vertical">
+        <div className="h-100">
+            <h6 className="text-center">{props.date}</h6>
+            <div className="list-group my-city-list h-100">
 
-                <button className="btn btn-primary">
-                    <div>
-                        {props.events.s.map(e => <div>
-                                <p>{e.name}</p>
-                                {(e.isMedal) ? <i class="fas fa-award"></i> : ""}
-                            </div>
-                        )}
-                    </div>
-                </button>
-                <button className="btn btn-primary">
-                    <div>
-                        {props.events.å.map(e => <div>
-                                <p>{e.name}</p>
-                                {(e.isMedal) ? <i class="fas fa-award"></i> : ""}
-                            </div>
-                        )}
-                    </div>
-                </button>
-                <button className="btn btn-primary">
-                    <div>
-                        {props.events.f.map(e => <div>
-                                <p>{e.name}</p>
-                                {(e.isMedal) ? <i class="fas fa-award"></i> : ""}
-                            </div>
-                        )}
-                    </div>
-                </button>
+            <button 
+                type="button" 
+                data-toggle="list" 
+                className={"list-group-item list-group-item-action list-group-item-danger active"}
+                style={{height:"50px"}}
+            >
+                
+        </button>
+                
+                    <CityButton
+                        city={"Stockholm"}
+                        day={Number.parseInt(props.date.substring(0,2))-6}
+                        events={props.events.s}
+                    />
+                    <CityButton
+                        city={"Åre"}
+                        day={Number.parseInt(props.date.substring(0,2))-6}
+                        events={props.events.å}
+                    />
+                    <CityButton
+                        city={"Falun"}
+                        day={Number.parseInt(props.date.substring(0,2))-6}
+                        events={props.events.f}
+                    />
             </div>
         </div>
     )
