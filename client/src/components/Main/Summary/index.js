882e4839e52f9a718b759b0e6cf8df71bd477170
Puya Alemirad <p.alemirad@gmail.com>
2019-06-05 15:39:18 +0200

    display selected routes in summary

--- client/src/components/Main/Summary/index.js
+++ client/src/components/Main/Summary/index.js
@@ -1,10 +1,35 @@
 import React from 'react'
+import Route from '../../Route'
 
 export default function Summary(props) {
     return (
-        <div>
-            HEj Summary
-            {console.log(props.routes)}
-        </div>
+               
+            <table id={`trip${props.dataKey}`} className="table">
+                <thead >
+                    <tr className="segment title">
+
+                        <th scope="col">From --> To </th>
+                        <th scope="col">Transport </th>
+                        <th scope="col">Duration </th>
+                        <th scope="col">Price({props.currencyCode}) </th>
+                        <th scope = "col"> Option </th>
+                    </tr>
+                </thead>
+                {props.routes.map((r, ri) => {
+                    return <Route
+                        key={ri}
+                        dataKey={ri}
+                        tripDataKey = {props.dataKey}
+                        currency={r.currency}
+                        segments={r.segments}
+                        duration={r.duration}
+                        price={r.price}
+                        origin={r.startPoint}
+                        destination={r.endPoint}
+                    />
+                })}
+                </table>
+
+        
     )
 }
